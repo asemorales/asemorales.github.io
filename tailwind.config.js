011fbafc936028e5e2cd02c9668775da6dcb4d6f
ase <irishdmorales@outlook.com>
2024-09-29 00:44:15 +0800

    Set up TailwindCSS

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './_data/**/*.yml',
+    './_includes/**/*.html',
+    './_layouts/**/*.html',
+    '*.html'
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
