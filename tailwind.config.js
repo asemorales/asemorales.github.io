dc34a69e557ced08a10e1328af2317e67ca35f97
ase <irishdmorales@outlook.com>
2024-09-29 03:33:11 +0800

    Style default dark colors & fonts w mobile margins

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,7 +7,18 @@ module.exports = {
     '*.html'
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'musubi': {
+          100: '#E1E6FF',
+          200: '#9AA5CE',
+          300: '#565F89',
+          400: '#181E36',
+          500: '#14192B',
+          600: '#0D1017',
+        }
+      }
+    },
   },
   plugins: [],
 }
