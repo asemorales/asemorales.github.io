68482c5209f29d24c4d1ef1c7c53cf842b626b14
ase <irishdmorales@outlook.com>
2024-10-05 19:21:44 +0800

    Animate toggle theme

--- assets/js/default.js
+++ assets/js/default.js
@@ -1,12 +1,12 @@
 window.onload = function() {
     if (window.matchMedia('(prefers-color-scheme: dark)').matches) {
-        document.querySelector('#moon').classList.toggle('invisible');
+        document.querySelector('#moon').classList.toggle('inactive-theme-icon');
     } else {
-        document.querySelector('#sun').classList.toggle('invisible');
+        document.querySelector('#sun').classList.toggle('inactive-theme-icon');
     }
     
     document.querySelector('#toggle-theme').addEventListener('click', function() {
-        document.querySelector('#moon').classList.toggle('invisible inactive-theme-icon');
-        document.querySelector('#sun').classList.toggle('invisible inactive-theme-icon');
+        document.querySelector('#moon').classList.toggle('inactive-theme-icon');
+        document.querySelector('#sun').classList.toggle('inactive-theme-icon');
     });
 };
