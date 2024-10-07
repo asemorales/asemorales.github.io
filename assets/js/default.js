4cc09116cf484d4038ea4d999009b3ab5a86b5ee
ase <irishdmorales@outlook.com>
2024-10-07 16:50:08 +0800

    Run formatter

--- assets/js/default.js
+++ assets/js/default.js
@@ -1,12 +1,12 @@
-window.onload = function() {
+window.onload = function () {
     if (window.matchMedia('(prefers-color-scheme: dark)').matches) {
         document.querySelector('html').classList.toggle('dark');
         document.querySelector('#moon').classList.toggle('inactive-theme-icon');
     } else {
         document.querySelector('#sun').classList.toggle('inactive-theme-icon');
     }
-    
-    document.querySelector('#toggle-theme').addEventListener('click', function() {
+
+    document.querySelector('#toggle-theme').addEventListener('click', function () {
         document.querySelector('html').classList.toggle('dark');
         document.querySelector('#moon').classList.toggle('inactive-theme-icon');
         document.querySelector('#sun').classList.toggle('inactive-theme-icon');
