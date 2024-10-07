09cedb249643df351be2d1bafef08a4c87eececa
ase <irishdmorales@outlook.com>
2024-10-07 17:41:10 +0800

    Show/hide nav site title on scroll

--- assets/js/default.js
+++ assets/js/default.js
@@ -1,4 +1,5 @@
 window.onload = function () {
+
     if (window.matchMedia('(prefers-color-scheme: dark)').matches) {
         document.querySelector('html').classList.toggle('dark');
         document.querySelector('#moon').classList.toggle('inactive-theme-icon');
@@ -6,9 +7,19 @@ window.onload = function () {
         document.querySelector('#sun').classList.toggle('inactive-theme-icon');
     }
 
-    document.querySelector('#toggle-theme').addEventListener('click', function () {
+    // listeners
+
+    document.querySelector('#toggle-theme').addEventListener('click', function() {
         document.querySelector('html').classList.toggle('dark');
         document.querySelector('#moon').classList.toggle('inactive-theme-icon');
         document.querySelector('#sun').classList.toggle('inactive-theme-icon');
     });
 };
+
+window.onscroll = function() {
+    if (document.querySelector('#about').getBoundingClientRect().bottom <= 0) {
+        document.querySelector('#nav-title').classList.remove('opacity-0');
+    } else {
+        document.querySelector('#nav-title').classList.add('opacity-0');
+    }
+}
