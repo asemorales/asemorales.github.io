b1bbd6362a04b77a5ff3114940e28e272a034f5b
ase <irishdmorales@outlook.com>
2024-10-07 15:55:05 +0800

    Tweak theme toggle

--- assets/js/default.js
+++ assets/js/default.js
@@ -1,14 +1,14 @@
-// window.onload = function() {
-//     if (window.matchMedia('(prefers-color-scheme: dark)').matches) {
-//         document.querySelector('html').classList.toggle('dark');
-//         document.querySelector('#moon').classList.toggle('inactive-theme-icon');
-//     } else {
-//         document.querySelector('#sun').classList.toggle('inactive-theme-icon');
-//     }
+window.onload = function() {
+    if (window.matchMedia('(prefers-color-scheme: dark)').matches) {
+        document.querySelector('html').classList.toggle('dark');
+        document.querySelector('#moon').classList.toggle('inactive-theme-icon');
+    } else {
+        document.querySelector('#sun').classList.toggle('inactive-theme-icon');
+    }
     
-//     document.querySelector('#toggle-theme').addEventListener('click', function() {
-//         document.querySelector('html').classList.toggle('dark');
-//         document.querySelector('#moon').classList.toggle('inactive-theme-icon');
-//         document.querySelector('#sun').classList.toggle('inactive-theme-icon');
-//     });
-// };
+    document.querySelector('#toggle-theme').addEventListener('click', function() {
+        document.querySelector('html').classList.toggle('dark');
+        document.querySelector('#moon').classList.toggle('inactive-theme-icon');
+        document.querySelector('#sun').classList.toggle('inactive-theme-icon');
+    });
+};
