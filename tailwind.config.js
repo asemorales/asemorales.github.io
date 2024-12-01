e6e0d6dd9a05cf04df3dd077b786314840607337
IrishMorales <irishdmorales@outlook.com>
2024-12-01 22:50:30 +0800

    Add and configure DaisyUI

--- tailwind.config.js
+++ tailwind.config.js
@@ -31,6 +31,36 @@ module.exports = {
       }
     },
   },
-  plugins: [],
+  daisyui: {
+    themes: [
+      {
+        dark: {
+          "primary": "#7dd3fc",
+          "primary-content": "#0D1017",
+          "secondary": "#fdba74",
+          "secondary-content": "#0D1017",
+          "accent": "#6ee7b7",
+          "accent-content": "#0D1017",
+          "neutral": "#0D1017",          
+          "neutral-content": "#E1E6FF",
+          "base-100": "#0D1017",
+          "base-200": "#14192B",
+          "base-300": "#181E36",
+          "base-content": "#E1E6FF",
+          "info": "#67e8f9",
+          "info-content": "#000a16",
+          "success": "#bef264",
+          "success-content": "#000904",
+          "warning": "#fcd34d",
+          "warning-content": "#160300",
+          "error": "#f87171",
+          "error-content": "#160404",
+        },
+      },
+    ],
+  },
+  plugins: [
+    require('daisyui'),
+  ],
 }
 
