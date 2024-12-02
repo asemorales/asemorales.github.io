a27597dad15194c4535668e61630ce7ed5a7b3ad
IrishMorales <irishdmorales@outlook.com>
2024-12-02 14:55:10 +0800

    Toggle theme icon

--- assets/js/default.js
+++ assets/js/default.js
@@ -1,15 +1,6 @@
 window.onload = function () {
     setThemeIcon(document.querySelector('html').getAttribute('data-theme'));
-    const mutationCallback = (mutationsList) => {
-        for (const mutation of mutationsList) {
-          if (mutation.type !== "attributes" || mutation.attributeName !== "data-theme") {
-            return
-          }
-          setThemeIcon(mutation.target.getAttribute("data-theme"));
-        }
-    };
-    const observer = new MutationObserver(mutationCallback);
-    observer.observe(document.querySelector('html'), { attributes: true })
+    watchTheme();
 };
 
 window.onscroll = function() {
@@ -20,14 +11,28 @@ window.onscroll = function() {
     }
 }
 
-function setThemeIcon(theme) {
-    console.log(theme);
+function setThemeIcon(theme) { // designed to support multiple (2+) themes
     switch(theme) {
         case 'light':
             document.querySelector('#sun').classList.remove('inactive-theme-icon');
             document.querySelector('#moon').classList.add('inactive-theme-icon');
+            break;
         case 'dark':
             document.querySelector('#moon').classList.remove('inactive-theme-icon');
             document.querySelector('#sun').classList.add('inactive-theme-icon');
+            break;
     }
+}
+
+function watchTheme() {
+    const mutationCallback = (mutationsList) => {
+        for (const mutation of mutationsList) {
+          if (mutation.type !== "attributes" || mutation.attributeName !== "data-theme") {
+            return
+          }
+          setThemeIcon(mutation.target.getAttribute("data-theme"));
+        }
+    };
+    const observer = new MutationObserver(mutationCallback);
+    observer.observe(document.querySelector('html'), { attributes: true })
 }
