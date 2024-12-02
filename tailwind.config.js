5c9a73bf856ed52635ed5258257d8d77fb7bc184
IrishMorales <irishdmorales@outlook.com>
2024-12-02 11:04:45 +0800

    Improve dark theme contrast

--- tailwind.config.js
+++ tailwind.config.js
@@ -82,16 +82,16 @@ module.exports = {
           "primary": "#7dd3fc", // sky-300
           "primary-content": "#E1E6FF",
           "--primary-low-contrast": "39.12% 0.0845 240.88", // sky-900
-          "--primary-high-contrast": "68.47% 0.1479 237.32", // sky-500
+          "--primary-high-contrast": "82.76% 0.1013 230.32", // sky-100
           "secondary": "#fdba74", // orange-300
           "secondary-content": "#E1E6FF",
           "--secondary-low-contrast": "40.84% 0.1165 38.17", // orange-900
-          "--secondary-high-contrast": "70.49% 0.1867 47.6", // orange-500
-          "accent": "#6ee7b7", // emerald-300
+          "--secondary-high-contrast": "83.66% 0.1165 66.29", // orange-100
+          "accent": "#f9a8d4", // pink-300
           "accent-content": "#E1E6FF",
-          "--accent-low-contrast": "37.8% 0.073 168.94", // emerald-900
-          "--accent-high-contrast": "69.59% 0.1491 162.48", // emerald-500
-          "neutral": "#0D1017",          
+          "--accent-low-contrast": "29.32% 0.1309 325.66", // pink-900
+          "--accent-high-contrast": "82.28% 0.1095 346.02", // pink-200
+          "neutral": "#0D1017",
           "neutral-content": "#E1E6FF",
           "base-100": "#0D1017",
           "base-200": "#14192B",
