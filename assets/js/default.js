3dc062fdcf86ee3196423dee948a5dce60a108fe
IrishMorales <irishdmorales@outlook.com>
2024-12-05 14:18:41 +0800

    Rename about to header for clarity

--- assets/js/default.js
+++ assets/js/default.js
@@ -4,7 +4,7 @@ window.onload = function () {
 };
 
 window.onscroll = function() {
-    if (document.querySelector('#about').getBoundingClientRect().bottom <= 0) {
+    if (document.querySelector('#header').getBoundingClientRect().bottom <= 0) {
         document.querySelector('#nav-title').classList.remove('opacity-0');
     } else {
         document.querySelector('#nav-title').classList.add('opacity-0');
