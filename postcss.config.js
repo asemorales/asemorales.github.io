0cce4423c95dffe8a096de87c48bdd4aba35965f
IrishMorales <irishdmorales@outlook.com>
2024-12-07 00:15:19 +0800

    WIP Setup PostCSS config

--- postcss.config.js
+++ postcss.config.js
@@ -1,14 +1,13 @@
 const path = require('path');
 
 module.exports = {
-    plugins: {
-        'postcss-import': {},
-        'postcss-url': {
-            url: 'copy',
-            basePath: path.resolve('node_modules/remixicon/fonts'),
-            assetsPath: 'assets/remixicon',
-        },
-        tailwindcss: {},
-        autoprefixer: {},
-    }
+  plugins: {
+    'postcss-import': {},
+    'postcss-url': {
+      url: 'copy',
+      basePath: path.resolve('node_modules/remixicon/fonts'),
+      assetsPath: 'assets/remixicon',
+    },
+    tailwindcss: {},
+  }
 }
