6236adccf326edc41faf3d1f907686cd4e5afc67
IrishMorales <irishdmorales@outlook.com>
2024-12-07 11:15:27 +0800

    Cleanup remixicon import

--- postcss.config.js
+++ postcss.config.js
@@ -3,11 +3,6 @@ const path = require('path');
 module.exports = {
   plugins: {
     'postcss-import': {},
-    'postcss-url': {
-      url: 'copy',
-      basePath: path.resolve('node_modules/remixicon/fonts'),
-      assetsPath: 'assets/remixicon',
-    },
     tailwindcss: {},
   }
 }
