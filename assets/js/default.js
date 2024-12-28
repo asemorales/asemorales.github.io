caeb85f26b221134ba54af46ac7e3c3b167f9ee3
IrishMorales <irishdmorales@outlook.com>
2024-12-28 19:27:29 +0800

    Blur navbar background on scroll

--- assets/js/default.js
+++ assets/js/default.js
@@ -1,7 +1,7 @@
 window.onscroll = function () {
   if (document.querySelector("#hero").getBoundingClientRect().top < 0) {
-    document.querySelector("#navbar").classList.add("bg-base-100/75");
+    document.querySelector("#navbar").classList.add("bg-base-100/75", "backdrop-blur-sm");
   } else {
-    document.querySelector("#navbar").classList.remove("bg-base-100/75");
+    document.querySelector("#navbar").classList.remove("bg-base-100/75", "backdrop-blur-sm");
   }
 };
