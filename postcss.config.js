4d7692704fdcbad7abc61b8e641c16263f3faf62
ase <42305156+asemorales@users.noreply.github.com>
2026-01-07 02:21:35 +0800

    fix: remove double run of purgecss

--- postcss.config.js
+++ postcss.config.js
@@ -5,12 +5,6 @@ module.exports = {
     tailwindcss: {},
     autoprefixer: {},
     ...(process.env.NODE_ENV === 'production' ? {
-      "@fullhuman/postcss-purgecss": {
-        content: ['./**/*.yml', './**/*.html', './assets/js/default.js'],
-        css: ['./assets/css/compiled.min.css'],
-        defaultExtractor: (content) => content.match(/[\w\-:\>.\/\[#%\]]+(?<!:)/g) || [], // Do not purge Tailwind classes with breakpoints (ex. sm:)
-        output: './assets/css/compiled.min.css',
-      },
       cssnano: {}
     } : {})
   },
