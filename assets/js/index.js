eff4e533a9f886d5a9500bfda64f2283ffcb8030
IrishMorales <asemorales.tech@gmail.com>
2026-01-07 00:58:31 +0800

    feat: clean up duplicate scripts

--- assets/js/index.js
+++ assets/js/index.js
@@ -2,8 +2,3 @@ document.getElementById('philosophy-show-more-btn').addEventListener("click", fu
     document.getElementById('philosophy-show-more-btn-wrapper').classList.add("hidden");
     document.getElementById('philosophy-part-2').classList.remove("hidden");
 });
-
-document.getElementById('about-me-show-more-btn').addEventListener("click", function (e) {
-    document.getElementById('about-me-show-more-btn-wrapper').classList.add("hidden");
-    document.getElementById('about-me-part-2').classList.remove("hidden");
-});
